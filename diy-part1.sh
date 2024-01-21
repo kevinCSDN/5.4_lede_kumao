4bc66bed22d0353272de89138b23d80d111b5ac3
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-21 13:49:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,9 +29,12 @@ rm -rf openwrt-packages
 rm -rf ./feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-bootstrap
 
 
-
+cd package
+git clone -b main https://github.com/sirpdboy/luci-theme-kucat.git
+cd ..
 
 
 
